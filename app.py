6ccee118c8325d17e22c76f950d7344ca10fe745
abhigyanvarma <119607855+abhigyanvarma@users.noreply.github.com>
2025-09-02 15:25:16 +0530

    Fix: use Render PORT and host=0.0.0.0

--- app.py
+++ app.py
@@ -463,7 +463,6 @@ def home():
 # Main
 # -------------------------
 if __name__ == '__main__':
-    # Make sure your Atlas "Network Access" allows your IP,
-    # and that the DB user dilli/dilli exists with proper roles.
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
 
